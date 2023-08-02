7e0452995a12e372b7c2ccd6ca7dc6df2ecfe089
manhduonghn <manhduonghn@live.com>
2023-08-02 20:27:29 +0700

    Create _config.py

---
+++ src/_config.py
@@ -0,0 +1,11 @@
+config = {
+    "dist_dir": "revanced-dist",
+    "keystore_path": "./keystore/default.keystore",
+}
+
+app_reference = {
+    "youtube": {
+        "name": "com.google.android.youtube",
+        "apkmirror": "https://www.apkmirror.com/apk/google-inc/youtube/youtube",
+    },
+}
