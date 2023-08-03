4e1468f03bc8d2376f8c7192376bb471ca00bba7
manhduonghn <manhduonghn@live.com>
2023-08-03 09:10:11 +0700

    Add some apps

--- src/_config.py
+++ src/_config.py
@@ -8,4 +8,25 @@ app_reference = {
         "name": "com.google.android.youtube",
         "apkmirror": "https://www.apkmirror.com/apk/google-inc/youtube/youtube",
     },
+    "youtube-music": {
+        "name": "com.google.android.apps.youtube.music",
+        "apkmirror": "https://www.apkmirror.com/apk/google-inc"
+        + "/youtube-music/youtube-music",
+    },
+    "twitter": {
+        "name": "com.twitter.android",
+        "apkmirror": "https://www.apkmirror.com/apk/twitter-inc/twitter/twitter",
+    },
+    "reddit": {
+        "name": "com.reddit.frontpage",
+        "apkmirror": "https://www.apkmirror.com/apk/redditinc/reddit/reddit",
+    },
+    "tiktok": {
+        "name": "com.ss.android.ugc.trill",
+        "apkmirror": "https://www.apkmirror.com/apk/tiktok-pte-ltd/tik-tok-including-musical-ly",
+    },
+    "twitch": {
+        "name": "tv.twitch.android.app",
+        "apkmirror": "https://www.apkmirror.com/apk/twitch-interactive-inc/twitch",
+    },
 }
